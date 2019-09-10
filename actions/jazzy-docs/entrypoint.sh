a0ecb91d315146bbd52420a7cc3367cd109e4905
Emil Marashliev <marashliev@gmail.com>
2019-09-10 11:23:59 +0300

    Update jazzy-docs entrypoint.sh

--- actions/jazzy-docs/entrypoint.sh
+++ actions/jazzy-docs/entrypoint.sh
@@ -3,7 +3,7 @@
 git config --global user.name = "Github Actions"
 git config --global user.email = "actions@github.com"
 
-git clone https://github.com/$GITHUB_REPOSITORY.git ./project
+git clone https://github.com/$GITHUB_REPOSITORY.git project
 
 cd project
 
@@ -11,13 +11,11 @@ swift build && \
 sourcekitten doc --spm-module $TARGET > $TARGET.json && \
 jazzy --clean --sourcekitten-sourcefile $TARGET.json --module $TARGET --output ../docs && \
 \
-git reset --hard HEAD^ && \
 git checkout -B gh-pages && \
-git pull origin gh-pages && \
-\
-mv .git ../docs && \
+git pull origin gh-pages &>/dev/null && \
+git reset --hard HEAD && \
+cp -r .git ../docs && \
 cd ../docs && \
-\
 git add . && \
 \
 git commit -m "Update Jazzy docs" && \
